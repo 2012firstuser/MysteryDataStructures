8fff9edc4b41648ace52f4da7a6680d8ef89b18d
Bryt12 <brysontang@gmail.com>
2017-11-18 20:49:58 -0500

    Added Add test

--- src/ExperimentRunner.java
+++ src/ExperimentRunner.java
@@ -9,7 +9,7 @@ public class ExperimentRunner {
 
 	public static void main (String[] args) {
 		final String cs210XTeamIDForProject4 = "ashaji"; // TODO CHANGE THIS TO THE TEAM ID YOU USE TO SUBMIT YOUR PROJECT3 ON INSTRUCT-ASSIST.
-
+		final int[] Ns = { 1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000 };
 		// Fetch the collections whose type you must deduce.
 		// Note -- you are free to change the type parameter from Integer to whatever you want. In this
 		// case, make sure to replace (over the next 4 lines of code) Integer with whatever class you prefer.
@@ -21,26 +21,36 @@ public class ExperimentRunner {
 			mysteryDataStructures[i] = MysteryDataStructure.getMysteryDataStructure(cs210XTeamIDForProject4.hashCode(), i, new Integer(0));
 		}
 
-		// Write your code here...
-		final Random random = new Random();  // instantiate a random number generator
-		final int N = 100;
-		for (int i = 0; i < N; i++) {  // populate the mystery data structure with 100 numbers
-			mysteryDataStructures[0].add(new Integer(i));
-		}
-		final int elementToFind = random.nextInt(N);
-
-		// This is an example of measuring an operation's time cost *without* averaging -- the times will vary wildly!		
-		// You really should average...
-		final long start = CPUClock.getNumTicks();
-		// Time how long it takes to find a single, randomly chosen item stored in the mystery data structure
-		final boolean result = mysteryDataStructures[0].contains(elementToFind);
-		final long end = CPUClock.getNumTicks();
-		final long elapsed = end - start;
-
 		// Write a table of numbers (for different N -- here, we are just showing one value for simplicity) showing
 		// the relationship between N and the time-cost associated with searching (with the contains method) through
 		// a collection of N data.
-		System.out.println("N\tT (contains(o))");
-		System.out.println(N + "\t" + elapsed);
+
+		for(int N = 0; N < Ns.length; N++) {
+			System.out.println(Ns[N]);
+			for (int i = 0; i < 5; i++) {
+				checkBSTAdd(mysteryDataStructures[i], Ns[N]);
+				mysteryDataStructures[i].clear();
+			}
+		}
+	}
+
+	public static void checkBSTAdd(Collection210X<Integer> c, int N){
+		int S = 5;
+		for(int i = 0; i < N + S; i++){
+			c.add(i);
+		}
+
+		final long startBest = CPUClock.getNumTicks();
+		c.add(S - 2);
+		final long endBest = CPUClock.getNumTicks();
+
+		final long startWorst = CPUClock.getNumTicks();
+		c.add(N + S + 2);
+		final long endWorst = CPUClock.getNumTicks();
+
+		System.out.println("Best:" + "\t" + (endBest-startBest));
+		System.out.println("Worst:" + "\t" + (endWorst-startWorst));
+
+
 	}
 }
