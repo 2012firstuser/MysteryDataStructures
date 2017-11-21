37e6307e0f34e1b32dfebab7788c3c697b484db5
Ankur Gupta <rukna1000@gmail.com>
2017-11-20 22:15:45 -0500

    Fixed the parser

--- src/CSVCreator.java
+++ src/CSVCreator.java
@@ -1,26 +1,35 @@
 import java.io.FileWriter;
 
 public class CSVCreator {
-    public static void exportDatum(int[][][] data, String testname) throws Exception
-    {
+    public static void exportDatum(int[][][] data, String testname) throws Exception  {
         for (int i = 0; i < data.length; i++) {
             exportData(data[i], testname, i);
         }
 
     }
-    public static void exportData(int[][] data, String testname, int datasetNum) throws Exception
-    {
-        String file = "/MysteryDataStructure" + datasetNum + "/" + testname + ".csv";
+    public static void exportData(int[][] data, String testname, int datasetNum) throws Exception {
+//        String file = "/MysteryDataStructure" + datasetNum + "/" + testname + ".csv";
+        String file = datasetNum + "_" + testname + ".csv";
+
         FileWriter writer = new FileWriter(file);
-        writer.write("n,Best Case,Worst Case,Average Case");
-        for(int k = 0; k < data[k].length;k++){
+
+        writer.write("n,Best Case,Worst Case\n");
+        for(int k = 0; k < data.length; k++){
+
             String nextline = "";
-            for(int j = 0; j<3;j++)
+
+            for(int j = 0; j <= data[0].length - 2;j++)
             {
-                nextline+= data[k][j] + ",";
+                nextline += data[k][j] + ",";
             }
+
+            nextline += data[k][data[0].length - 1];
+            nextline += "\n";
+
             writer.write(nextline);
         }
+
+        writer.close();
     }
 }
 
